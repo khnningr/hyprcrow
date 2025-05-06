2f53939255cc50ef79e0672747a443ff25b379b5
khnningr <barrios12khnningr@proton.me>
2025-05-05 23:51:22 -0600

    ->2025/05/05 a las 11:51 p.m.

--- .local/bin/terminal.sh
+++ .local/bin/terminal.sh
@@ -3,11 +3,11 @@
 # Colors from 0-15
 printf "\033]4;0;#181818\033\\"
 printf "\033]4;1;#ffb4ab\033\\"
-printf "\033]4;2;#c1c1ff\033\\"
-printf "\033]4;3;#575992\033\\"
-printf "\033]4;4;#c1c1ff\033\\"
-printf "\033]4;5;#e9b9d3\033\\"
-printf "\033]4;6;#c6c4dd\033\\"
+printf "\033]4;2;#84d6c3\033\\"
+printf "\033]4;3;#046b5c\033\\"
+printf "\033]4;4;#84d6c3\033\\"
+printf "\033]4;5;#abcae4\033\\"
+printf "\033]4;6;#b1ccc4\033\\"
 printf "\033]4;7;#BAC2DE\033\\"
 printf "\033]4;8;#585B70\033\\"
 printf "\033]4;9;#F38BA8\033\\"
@@ -19,13 +19,13 @@ printf "\033]4;14;#94E2D5\033\\"
 printf "\033]4;15;#A6ADC8\033\\"
 
 # Background
-printf "\033]11;#131318\033\\";
+printf "\033]11;#0e1513\033\\";
 
 # Foreground
-printf "\033]10;#e4e1e9\033\\";
+printf "\033]10;#dee4e1\033\\";
 
 # Cursor
-printf "\033]12;#e4e1e9\033\\";
+printf "\033]12;#dee4e1\033\\";
 
 # Border
-printf "\033]7044;#47464f\033\\";
+printf "\033]7044;#3f4946\033\\";

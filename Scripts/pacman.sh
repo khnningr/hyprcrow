efd27fd6eac5eab3059a616d642d516b93d0040c
khnningr <barrios12khnningr@proton.me>
2025-06-04 00:20:11 -0600

    Cambios a scripts.

--- Scripts/pacman.sh
+++ Scripts/pacman.sh
@@ -26,8 +26,8 @@ if ! grep -q "chaotic-aur" /etc/pacman.conf; then
     sudo pacman-key --lsign-key 3056513887B78AEB
     sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
     sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-    sudo pacman -Syu
     sudo cp ./pacman.conf /etc/pacman.conf
+    sudo pacman -Sy
 fi
 
 # rate-mirrors

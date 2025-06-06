128c653cbba386284160214f7ba7a226bd7b2f3d
khnningr <barrios12khnningr@proton.me>
2025-06-06 14:44:01 -0600

    Script de firewalld.

---
+++ Scripts/firewalld_config.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if ! command -v firewalld &> /dev/null; then
+    . ./firewalld.sh
+fi
+
+if ! systemctl list-units --type=service | grep -q "firewalld.service"; then
+    systemctl enable --now firewalld
+fi
+
+if ! firewall-cmd --get-zones | grep -qw "crow"; then
+    sudo firewall-cmd --new-zone=crow --permanent
+    sudo systemctl restart firewalld
+fi
+
+if ! firewall-cmd --get-default | grep -qw "crow"; then
+    ip addr | grep "enp" # Ver la interface del equipo.
+
+    echo -e "\nEscriba la interface de la máquina"
+    echo "...dada por 'ip addr'"
+    read -p "> " elegir_interface
+
+    if [[ -z $elegir_interface ]]; then
+        echo "Interface no definida... vuelva intentarlo..."
+        exit 1
+    fi
+
+    sudo firewall-cmd --zone=crow --change-interface=${elegir_interface}
+    sudo firewall-cmd --set-default=crow
+fi
+
+puertos=(
+    "53"
+
+)
+
+# HTTPS
+sudo firewall-cmd --add-service=https
+# DNS
+sudo firewall-cmd --add-port=53/tcp
+# Warframe
+sudo firewall-cmd --add-port=4950/tcp
+sudo firewall-cmd --add-port=4955/tcp
+sudo firewall-cmd --add-port=6695/tcp
+sudo firewall-cmd --add-port=6696/tcp
+sudo firewall-cmd --add-port=6697/tcp
+sudo firewall-cmd --add-port=6698/tcp
+sudo firewall-cmd --add-port=6699/tcp
+# torrents
+sudo firewall-cmd --add-port=51413/tcp
+# Block ping
+sudo firewall-cmd --add-icmp-block=echo-request
+
+sudo firewall-cmd --runtime-to-permanent
+
+sudo firewall-cmd --reload
+sudo systemctl restart firewalld
--- Scripts/virt_manager.sh
+++ Scripts/virt_manager.sh
@@ -7,13 +7,17 @@ if command -v pacman &> /dev/null; then
     sudo pacman -S --needed "${pkg_virt_manager[@]}"
 fi
 
+if command -v dnf &> /dev/null; then
+    sudo dnf install @virtualization
+fi
+
 # Descomentar las lineas 85 y 108.
 # unix_socket_group = "libvirt"
 # unix_socket_rw_perms = "0770"
 # sudo nano /etc/libvirt/libvirtd.conf
-sudo sed -i 's/#unix_socket_rw_perms = "0770"/unix_socket_rw_perms = "0770"/' /etc/libvirt/libvirtd.conf
-sudo sed -i 's/#unix_socket_group = "libvirt"/unix_socket_group = "libvirt"/' \
-  /etc/libvirt/qemu.conf
+sudo sed -i 's/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/' /etc/libvirt/libvirtd.conf
+sudo sed -i 's/#unix_sock_group = "libvirt"/unix_socket_group = "libvirt"/' \
+  /etc/libvirt/libvirtd.conf
 
 # Añadir el usuario actual al grupo kvm y libvirt.
 sudo usermod -a -G kvm,libvirt $(whoami)
@@ -44,8 +48,7 @@ sudo virsh net-autostart default
 # Habilitar la virtualización con el firewall activo.
 # Descomentar línea 29.
 # firewall_backend = "nftables"
-# sudo sed -i 's/^#firewall_backend = "iptables"/firewall_backend = "nftables"/' /etc/libvirt/network.conf
-
+#sudo sed -i 's/^#firewall_backend = "nftables"/firewall_backend = "iptables"/' /etc/libvirt/network.conf
 
 # Comprobar que se han habilitado los servicios.
 # sudo systemctl status libvirtd.service

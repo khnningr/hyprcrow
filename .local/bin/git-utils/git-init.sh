5910dcac4d259791e6fea9d5821f1488d182c775
khnningr <barrios12khnningr@proton.me>
2025-11-03 00:50:10 -0600

    2025/11/03 a las 12:50 a.m.

--- .local/bin/git-utils/git-init.sh
+++ .local/bin/git-utils/git-init.sh
@@ -5,12 +5,14 @@
 
 source login.sh
 
+echo "Importante: Debes crear primero el repositorio desde la web de github"
+
 DIR_NUEVO_REPOSITORIO=$(pwd)
 USER_NAME=$(git config --global --get user.name)
 
 echo -e "\nhttps://github.com/${USER_NAME}/REPO.git"
 
-echo "Elija el nombre del nuevo repositorio"
+echo "Nombre del nuevo repositorio"
 NOMBRE_REPO=$(gum input --placeholder "https://github.com/${USER_NAME}/»REPOSITORIO«.git")
 
 NUEVO_REPO="https://github.com/${USER_NAME}/${NOMBRE_REPO}.git"
@@ -24,7 +26,7 @@ git init
 source commit.sh "${NOMBRE_REPO}"
 
 git branch -M main
-git remote add origin "https://github.com/${USER_NAME}/${NUEVO_REPO}.git"
+git remote add origin "${NUEVO_REPO}"
 
 source push.sh
 

3d7a30cbbca9f1031b89e6dc2c55d7852bf385f9
khnningr <barrios12khnningr@proton.me>
2025-11-03 00:38:57 -0600

    2025/11/03 a las 12:38 a.m.

--- .local/bin/git-utils/push.sh
+++ .local/bin/git-utils/push.sh
@@ -2,11 +2,6 @@
 
 source login.sh
 
-if [[ -z $(git status --porcelain) ]]; then
-	echo -e "Verifica que haya algún archivo que subir."
-	return 1
-fi
-
 if [ "$(git symbolic-ref --short HEAD)" = "main" ]; then
 	git push -u origin main
 	echo "$REPOSITORIO actualizado correctamente."

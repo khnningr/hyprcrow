dade422f60b335a728cc2bdc1696681d0606f15c
khnningr <barrios12khnningr@proton.me>
2025-11-05 02:25:38 -0600

    2025/11/05 a las 02:25 a.m.

--- .local/bin/git-utils/dir-repos.sh
+++ .local/bin/git-utils/dir-repos.sh
@@ -2,9 +2,21 @@
 
 set -euo pipefail
 
-REPOS=(
+DIR_REPOS=(
 	"${HOME}/hyprcrow/"
 	"${HOME}/Wallpapers/"
 	"${HOME}/kh-home"
+)
+
+REPOS=(
+	"hyprcrow"
+	"Wallpapers"
+	"kh-home"
+)
+
+declare -A repositorios=(
+	["hyprcrow"]="${HOME}/hyprcrow/"
+	["Wallpapers"]="${HOME}/Wallpapers/"
+	["kh-home"]="${HOME}/kh-home"
 )
 
